dc43b53e7daa68f4f101aee43ca757f71e4fddf0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-02 01:56:52 +0700

    response object

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpCode, Res } from "@nestjs/common";
 
 // routing nya taru di decorator controller di mana dikelompokkan berdasarkan jalur routingnya
 @Controller("hero")
 export class HeroController {
     @Get("index")
-    index() {
-        return "Hero index";
+    @HttpCode(200)
+    index(@Res() response) {
+        response.json({
+            title: "Hero Index"
+        });
     }
 
     @Get("create")
