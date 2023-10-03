860cca9f80bcd34c0931794abe7ac1c344891220
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-04 02:00:48 +0700

    role resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroController } from './hero/hero.controller';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [],
+  imports: [RoleModule],
   controllers: [AppController, HeroController],
   providers: [AppService],
 })
