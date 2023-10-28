ff02d091299e662a341586743372baf4c878b830
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-28 15:04:16 +0700

    import user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoleModule } from './role/role.module';
 import { HeroModule } from './hero/hero.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [RoleModule, HeroModule],
+  imports: [RoleModule, HeroModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
