c9605cfc839b819e63e7962bb8588adfdefad032
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-31 00:53:51 +0700

    register auth module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { RoleModule } from './role/role.module';
 import { HeroModule } from './hero/hero.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [RoleModule, HeroModule, UserModule],
+  imports: [RoleModule, HeroModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
