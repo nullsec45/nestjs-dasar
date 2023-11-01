b2876be601fe3f4a6297e060226f3ed617b9f25f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-01 23:24:11 +0700

    m:compare pasword and jwt service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,30 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from "src/user/user.service";
 import { AuthDto } from './dto/auth.dto';
+import { compare } from "bcryptjs";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) { }
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService
+    ) { }
 
     async login(authDto: AuthDto) {
         const user = await this.userService.findByEmail(authDto.email)
 
-        if (user) {
+        if (user && (await compare(authDto.password, user.password))) {
             const { password, ...result } = user;
-            return result;
-        }
+            const payload = { sub: result.id, username: result.email }
+            return {
+                ...result,
+                access_token: await this.jwtService.signAsync(payload, {
+                    secret: 'Kopihtiam645_',
+                    expiresIn: '60S'
+                })
+            }
+        };
 
         throw new UnauthorizedException();
     }
